5c220266fa312f2abd71b11ab79d16a929794beb
gunhee <gunhee.jo.dev@gmail.com>
2026-05-19 17:53:01 +0900

    feat: 테스트 커버리지 도입

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,13 @@ module.exports = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
   },
+  collectCoverageFrom: [
+    "<rootDir>/**/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/**/node_modules/**",
+    "!<rootDir>/**/.expo/**",
+    "!<rootDir>/app-example/**",
+    "!<rootDir>/coverage/**",
+  ],
+  coverageReporters: ["text-summary", "lcov", "json-summary"],
   testPathIgnorePatterns: ["/node_modules/", "/.expo/"],
 };
